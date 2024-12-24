998dca4db0f06db2b992b13c79ec7438284c6d54
GiwrgosGln <gergegl1999@gmail.com>
2024-12-24 17:31:41 +0200

    feat: added requests limiter for brute force attacks for login

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,11 +1,16 @@
 import express, { Response } from "express";
 import { AuthController } from "../controllers/authController";
 import { AuthRequest } from "../types/auth";
+import { authLimiter } from "../middleware/rateLimiter";
 
 const router = express.Router();
 
 router.post("/register", AuthController.register as express.RequestHandler);
-router.post("/login", AuthController.login as express.RequestHandler);
+router.post(
+  "/login",
+  authLimiter,
+  AuthController.login as express.RequestHandler
+);
 router.post("/refresh", AuthController.refresh as express.RequestHandler);
 router.post("/logout", AuthController.logout);
 
