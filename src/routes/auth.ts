29e29f7c71abdccc5278987a53c99f707851171f
GiwrgosGln <gergegl1999@gmail.com>
2024-12-24 17:27:01 +0200

    feat: added protected route

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,5 +1,6 @@
-import express from "express";
+import express, { Response } from "express";
 import { AuthController } from "../controllers/authController";
+import { AuthRequest } from "../types/auth";
 
 const router = express.Router();
 
@@ -7,4 +8,9 @@ router.post("/register", AuthController.register as express.RequestHandler);
 router.post("/login", AuthController.login as express.RequestHandler);
 router.post("/refresh", AuthController.refresh as express.RequestHandler);
 router.post("/logout", AuthController.logout);
+
+const authHandler = (req: AuthRequest, res: Response) => {
+  const user = req.user;
+};
+
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth";
+import protectedRoutes from "./routes/protectedRoute";
 import cookieParser from "cookie-parser";
 
 dotenv.config();
@@ -14,6 +15,7 @@ app.use(cookieParser());
 
 // Routes
 app.use("/api/auth", authRoutes);
+app.use("/api", protectedRoutes);
 
 // Health check route
 app.get("/health", (req, res) => {
